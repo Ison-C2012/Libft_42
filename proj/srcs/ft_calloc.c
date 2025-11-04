61330d3cfed9e139a0ca7e003115246a3359e7a3
ison <ison.c.2012.tk@gmail.com>
2025-11-04 21:34:06 +0900

    edit calloc

--- proj/srcs/ft_calloc.c
+++ proj/srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 21:51:17 by keitotak          #+#    #+#             */
-/*   Updated: 2025/11/02 21:17:51 by keitotak         ###   ########.fr       */
+/*   Updated: 2025/11/04 17:35:45 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*mem;
 	size_t	m_size;
 
-	if (nmemb == 0 || size == 0)
-		return (malloc(0));
-	if (nmemb > SIZE_MAX / size)
+	if (size != 0 || nmemb > SIZE_MAX / size)
 		return (NULL);
 	m_size = nmemb * size;
 	mem = malloc(m_size);
