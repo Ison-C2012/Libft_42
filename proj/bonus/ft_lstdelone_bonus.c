7350a44dbcb48261c40871dbc8f4931035f38024
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2025-11-13 11:50:00 +0900

    bonus

--- proj/bonus/ft_lstdelone_bonus.c
+++ proj/bonus/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 17:59:51 by keitotak          #+#    #+#             */
-/*   Updated: 2025/10/28 21:44:04 by keitotak         ###   ########.fr       */
+/*   Updated: 2025/11/13 10:40:50 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (del)
-		del(lst->content);
+	del(lst->content);
 	free(lst);
 }
