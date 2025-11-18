225f4cb4a25ea3f5b87ef4ca05fa366136285243
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2025-11-19 01:32:27 +0900

    libft for push swap

--- proj/srcs/ft_lstclear_bonus.c
+++ proj/srcs/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 18:56:46 by keitotak          #+#    #+#             */
-/*   Updated: 2025/11/02 21:37:31 by keitotak         ###   ########.fr       */
+/*   Updated: 2025/11/19 01:27:50 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 
 	while (lst != NULL)
 	{
-		tmp = *lst->next;
+		tmp = (*lst)->next;
 		ft_lstdelone(*lst, del);
 		*lst = tmp;
 	}
